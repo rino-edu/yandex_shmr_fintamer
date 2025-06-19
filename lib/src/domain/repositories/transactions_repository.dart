e70c30182ab30fc6928140bcdf4876874e0895ff
r.gilmutdinov <rino64222@gmail.com>
2025-06-19 18:31:19 +0500

    add /transactions/{id} repo

--- lib/src/domain/repositories/transactions_repository.dart
+++ lib/src/domain/repositories/transactions_repository.dart
@@ -3,12 +3,15 @@ import 'package:fintamer/src/domain/models/transaction_response.dart';
 import 'package:fintamer/src/domain/models/requests/transaction_request.dart';
 
 abstract class ITransactionsRepository {
-
   Future<List<TransactionResponse>> getTransactionsForPeriod({
     required int accountId,
     DateTime? startDate,
     DateTime? endDate,
   });
+
+  Future<List<TransactionResponse>> getTransactionsByAccountId({
+    required int accountId,
+  }) => getTransactionsForPeriod(accountId: accountId);
 
   Future<Transaction> createTransaction({required TransactionRequest request});
 
